1f419ea23004276535e213e85dc873f2a425c38f
AndreyAttr <andrii.lehuienko@outlook.com>
2026-07-11 17:36:41 +0300

    clean

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-//123
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
